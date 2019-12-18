9fa61485250aeb1dee82d698838cabc7f4f77962
iskona <isony.d@gmail.com>
2019-12-18 13:43:46 -0800

    added two functions to check if generated password meets all criteria

--- script.js
+++ script.js
@@ -21,38 +21,76 @@ var getCharType = function () {
     capital = confirm("Do you want your password contains Upper case characters?");
 
     if (special === false && number === false && lowercase === false && capital === false) {
-        alert("It looks like you didn't choose ane of character type. Please try again.");
+        alert("It looks like you didn't choose ane of character type. Please refresh the page and try again.");
         getCharType();
     };
 }
 getCharType();
 
+
 var allNumbers = "0123456789";
-var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var specChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 var wholeCharSet;
 if (special === true) {
-    wholeCharSet += specChar;
+    wholeCharSet = specChar;
 }
+
 if (number === true) {
     wholeCharSet += allNumbers;
 }
+
 if (lowercase === true) {
     wholeCharSet += lowerCaseLetters;
 }
+
 if (capital === true) {
     wholeCharSet += upperCaseLetters;
 }
 
+function hasCharsFromSet(password, charSet) {
+    for (let i = 0; i < password.length; i++) {
+        for (let j = 0; j < charSet.length; j++) {
+            if (password[i] === charSet[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function badPassword(password) {
+    if (special === true && !hasCharsFromSet(password, specChar)) {
+        return true;
+    }
+
+    if (number === true && !hasCharsFromSet(password, allNumbers)) {
+        return true;
+    }
+
+    if (lowercase === true && !hasCharsFromSet(password, lowerCaseLetters)) {
+        return true;
+    }
+
+    if (capital === true && !hasCharsFromSet(password, upperCaseLetters)) {
+        return true;
+    }
+
+    return false;
+}
+
 function generatePswd() {
     var textArea = document.getElementById('password');
-    var myPassword = "";
-    for (var i = 0; i < pswdLength; i++) {
-        myPassword += wholeCharSet[Math.floor(Math.random() * wholeCharSet.length)];
-    }
-    console.log("Your password is: " + myPassword);
+    var myPassword;
+    do {
+        myPassword = "";
+        for (var i = 0; i < pswdLength; i++) {
+            myPassword += wholeCharSet[Math.floor(Math.random() * wholeCharSet.length)];
+        }
+        console.log("Your candidate password is: " + myPassword);
+    } while (badPassword(myPassword));
     textArea.textContent = myPassword;
 };
 
@@ -63,3 +101,4 @@ function copyText() {
     document.execCommand("copy");
     alert("Copied the text: " + copyText.value);
 };
+
